914acd5e56559894c39e097f43e517d89d7b8919
AyrielNoriega <anoriega2310@gmail.com>
2023-05-24 20:05:12 -0500

    add firts test

---
+++ tests/Feature/ProductControllerTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Product;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ProductControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    public function test_index()
+    {
+        Product::factory()->count(5)->create(); 
+
+        $response = $this->getJson('/api/products');
+
+        $response->assertSuccessful();
+        $response->assertHeader('content-type', 'application/json');
+        $response->assertJsonCount(5);
+    }
+
+}
